3545e85095d535c9fab5e8355ea8b81b5ff28d0f
nkwn <npquan2704@gmail.com>
2023-08-03 19:00:49 +0700

    Update receiving data method & Add behaviour for error codes

--- proxy.py
+++ proxy.py
@@ -5,60 +5,6 @@ import threading
 import sys
 import os
 
-if len(sys.argv) <= 1:
-    print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server]')
-    sys.exit(2)
-
-def handle_client(client_socket):
-    try:
-        request = client_socket.recv(4096)
-        method, url, host_name = get_server_info(request)
-        if method == None:
-            client_socket.close()
-            return
-        
-        if not is_in_access_time() or not is_whitelisted(host_name) or method not in ['GET', 'POST', 'HEAD']:
-            response = "HTTP/1.1 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
-            client_socket.sendall(response.encode())
-            client_socket.close()
-            return
-        
-        print(url)
-        if is_image_request(url):
-            _, path = os.path.split(url)
-            file_name = "cache/" + host_name.replace(".", "_") + "-" + path
-            if os.path.exists(file_name):
-                response = "HTTP/1.1 200 OK\r\n\r\n"
-                with open(file_name, "rb") as file:
-                    image_data = file.read()
-                client_socket.sendall(response.encode())
-                client_socket.sendall(image_data)
-                client_socket.close()
-                return
-            else:
-                image_data, server_respone = get_image_data_respone(host_name, request)
-                if image_data != b'':
-                    cache_image(host_name, image_data, url)
-                client_socket.sendall(server_respone)
-                client_socket.close()
-                return
-                
-        server_respone = get_server_respone(host_name, request)
-        client_socket.sendall(server_respone)
-        client_socket.close()
-    except OSError:
-        client_socket.close()
-
-def get_server_info(request):
-    try:
-        request_data = request.decode()
-        method = request_data.split(' ')[0]
-        url = request_data.split(' ')[1]
-        host_name = url.split('/')[2]
-        return method, url, host_name
-    except:
-        return None, None, None
-    
 def is_in_access_time():
     now = datetime.datetime.now()
     access_start = now.replace(hour=TIME_LIMIT[0],minute=0,second=0,microsecond=0)
@@ -77,6 +23,76 @@ def is_image_request(url):
     _, ext = os.path.splitext(url)
     return any(ext == ex for ex in image_extensions)
 
+def handle_client(client_socket):
+    try:
+        request = client_socket.recv(4096)
+        print(request)
+        method, url, host_name = get_server_info(request)
+        
+        print(url)
+        if method == None:
+            client_socket.close()
+            return
+        
+        if not is_in_access_time() or not is_whitelisted(host_name) or method not in ['GET', 'POST', 'HEAD']:
+            response = "HTTP/1.1 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
+            client_socket.sendall(response.encode())
+            client_socket.close()
+            return
+        
+        print(host_name)
+        if is_image_request(url):
+            _, path = os.path.split(url)
+            file_name = "cache/" + host_name.replace(".", "_") + "-" + path
+            if os.path.exists(file_name):
+                response = "HTTP/1.1 200 OK\r\n\r\n"
+                with open(file_name, "rb") as file:
+                    image_data = file.read()
+                client_socket.sendall(response.encode())
+                client_socket.sendall(image_data)
+                client_socket.close()
+                return
+            else:
+                image_data, server_response = get_image_data_respone(host_name, request)
+                if image_data != b'':
+                    cache_image(host_name, image_data, url)
+                client_socket.sendall(server_response)
+                client_socket.close()
+                return
+        
+        server_response = get_server_respone(host_name, request)
+        client_socket.sendall(server_response)
+        client_socket.close()
+    except OSError:
+        client_socket.close()
+
+def get_server_info(request):
+    try:
+        request_data = request.decode()
+        method = request_data.split(' ')[0]
+        url = request_data.split(' ')[1]
+        host_name = url.split('/')[2]
+        return method, url, host_name
+    except:
+        return None, None, None
+
+def get_status(server_respone):
+    buffer = server_respone.split(b'\r\n')[0]
+    status = buffer.split(b' ')[1]
+    return status
+
+def get_connection_close(server_respone):
+    connection = "connection: close" in server_respone.decode().lower()
+    return connection
+
+def get_image_data_respone(host_name, request):
+    server_respone = get_server_respone(host_name, request)
+    if get_status(server_respone) in error_codes:
+        return b'', server_respone
+    
+    image_data = server_respone.split(b"\r\n\r\n")[1]
+    return image_data, server_respone
+
 def get_server_respone(host_name, request):
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.connect((host_name, 80))
@@ -87,6 +103,18 @@ def get_server_respone(host_name, request):
     header_end = server_respone.find(b"\r\n\r\n")
     headers = server_respone[:header_end]
     
+    if get_status(server_respone) in error_codes:
+        server_respone = "HTTP/1.1 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
+        return server_respone.encode()
+    
+    if (get_connection_close(headers)):
+        while True:
+            data_chunk = server_socket.recv(1024)
+            if (data_chunk):
+                server_respone += data_chunk
+            else:
+                return server_respone
+
     if header_end + 4 != len(server_respone):    
         chunked_encoding = "transfer-encoding: chunked" in headers.decode().lower()
         content_length = get_content_length(headers)
@@ -107,6 +135,7 @@ def get_server_respone(host_name, request):
                     if end_check in data_chunks:
                         break
     server_socket.close()
+
     return server_respone
 
 def get_content_length(headers):
@@ -116,11 +145,6 @@ def get_content_length(headers):
             length = line.split(b":")[1].strip()
             return int(length)
     return 0
-
-def get_image_data_respone(host_name, request):
-    server_respone = get_server_respone(host_name, request)
-    image_data = server_respone.split(b"\r\n\r\n")[1]
-    return image_data, server_respone
 
 def cache_image(host_name, image_data, url):
     _, path = os.path.split(url)
@@ -135,6 +159,7 @@ def cache_clean():
         current_time = time.time()
         for file in os.listdir("./cache"):
             if current_time - images_cache_time[file] > CACHE_TIME:
+                print('recaching')
                 os.remove(os.path.join("cache/", file))
                 recache_image(file)
 
@@ -159,18 +184,21 @@ def get_config():
     return CACHE_TIME, WHITE_LIST, TIME_LIMIT
    
 def main():     
+    if len(sys.argv) <= 1:
+        print('Usage : "python proxy.py [server_ip]"\n[server_ip : IP address of proxy server]')
+        sys.exit(2)
+    
     tcpSerSock = socket(AF_INET, SOCK_STREAM)
     tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     tcpSerSock.bind((sys.argv[1], 8888))
-    tcpSerSock.listen(2)
-    print('Proxy Server is ready to receive connections...')
+    tcpSerSock.listen(WEB_CLIENT)
+    print('Proxy server is ready to receive connections...')
 
     if not os.path.exists("./cache"):
         os.makedirs("./cache")
 
     cache_clean_thread = threading.Thread(target=cache_clean, daemon=True)
     cache_clean_thread.start()
-    
     
     while True:
         try:
@@ -188,5 +216,7 @@ def main():
     
 
 CACHE_TIME, WHITE_LIST, TIME_LIMIT = get_config()
+WEB_CLIENT = 5
+error_codes = [b'405', b'404', b'403', b'401', b'400', b'408', b'500', b'502', b'503', b'100']
 images_cache_time = {}
 main()
